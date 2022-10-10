be0b2bc45d69c3565f2864817ef86457fdb6fbfb
behmjw <behmjw@mail.uc.edu>
2022-10-10 10:52:35 -0400

    Server

---
+++ Node/server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const os = require("os");
+const ip = require('ip');
+
+http.createServer((req, res) => {
+  if (req.url === "/") {
+      fs.readFile("./public/index.html", "UTF-8", (err, body) => {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(body);
+    });
+  } else if(req.url.match("/sysinfo")) {
+    myHostName=os.hostname();
+    html=`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Node JS Response</title>
+      </head>
+      <body>
+        <p>Hostname: ${myHostName}</p>
+        <p>IP: ${ip.address()}</p>
+        <p>Server Uptime: Days: ${Math.round(os.uptime() / 86400)} Hours: ${Math.round(os.uptime() / 3600)} Seconds: ${Math.round(os.uptime() / 60)} </p>
+        <p>Total Memory: ${os.totalmem() / 1000000} MB</p>
+        <p>Free Memory: ${os.freemem() / 1000000} MB</p>
+        <p>Number of CPUs: ${os.cpus().length}</p>
+      </body>
+    </html>`
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(html);
+  } else {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.end(`404 File Not Found at ${req.url}`);
+  }
+}).listen(3000);
+
+console.log("Server listening on port 3000");
